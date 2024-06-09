2500cca665874f91b8309bbe8e17c2ab77280d22
durgesh shukla <130758819+jammynoah@users.noreply.github.com>
2024-06-09 17:48:57 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API=axios.create({baseURL:'http://localhost:4000'})
+const API=axios.create({baseURL:'https://stack-overflow-wkfn.onrender.com'})
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("Profile")) {
       req.headers.authorization = `Bearer ${
